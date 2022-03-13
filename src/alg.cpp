6f07ee1449ca4531619e22ffe7e1727b5840c6dc
Кочеткова Екатерина <94628140+EkaKochetkova@users.noreply.github.com>
2022-03-13 23:05:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-int low = 0;           
-    int top = size - 1;   
+int low = 0;
+int top = size - 1;
     while (low <= top) {
         int mid = (top + low) / 2;
         if (arr[mid] == value) {
-            return mid + 1;
-        } else if (arr[mid] < value) {
+            int i = 1;
+            int j = 1;
+            int count = 1;
+            while ((arr[mid + i] == value || arr[mid - j] == value) && ((mid + i <= size) && (mid - j >= 0))) {
+                if (arr[mid + i] == value) {
+                    count += 1;
+                    i += 1;
+                }
+                if (arr[mid - j] == value) {
+                    count += 1;
+                    j += 1;
+                }
+            }
+            return count;
+        }
+        else if (arr[mid] < value) {
             low = low + 1;
         }
         else if (arr[mid] > value) {
